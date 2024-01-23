c0d7a2e34790cd95fdc2f00fd66c87794580dd0b
AdamFitzpatrick1 <31987448+AdamFitzpatrick1@users.noreply.github.com>
2024-01-23 17:50:04 +0000

    Added database information to the table

--- finance-tracker/src/main/java/financetracker/Controller/Home.java
+++ finance-tracker/src/main/java/financetracker/Controller/Home.java
@@ -1,7 +1,13 @@
 package financetracker.Controller;
 
+import financetracker.domain.Expenses;
+import financetracker.repositories.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Home {
@@ -11,4 +17,17 @@ public class Home {
         return "index.html";
     }
 
+    @GetMapping("/testing.html")
+    public String testing(){return "testing.html";}
+
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @GetMapping("/expenses")
+    public String listExpenses(Model model) {
+        List<Expenses> expenses = (List<Expenses>) expenseRepository.findAll();
+        model.addAttribute("expenses", expenses);
+        return "index.html";
+    }
 }
